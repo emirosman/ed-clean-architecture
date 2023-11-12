42c0d46e1ead3b52a2970f7255fd81fb187cf390
emirosman <m.emirosman@gmail.com>
2023-11-13 00:59:24 +0300

    Create Brand CQRS Implementation

--- nArchitectureDemo/src/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ nArchitectureDemo/src/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
 
-namespace Application.Features.Brands.Commands.Create
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>
 {
-    internal class CreateBrandCommand
+    public string Name { get; set; }
+
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
     {
+        public async Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
+            createdBrandResponse.Name = request.Name;
+            createdBrandResponse.Id = new Guid();
+            return createdBrandResponse;
+        }
     }
 }
