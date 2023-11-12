0acacd31b7cfe86dfcdf80a03f1083dd74d2ddf6
emirosman <m.emirosman@gmail.com>
2023-11-13 00:10:43 +0300

    Create Entity

---
+++ nArchitectureDemo/src/Core.Packages/Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Repositories;
+
+public class Entity<TId>
+{
+    public TId Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime UpdatedDate { get; set; }
+    public DateTime DeletedDate { get; set; }
+
+    public Entity()
+    {
+        Id = default;
+    }
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+}
