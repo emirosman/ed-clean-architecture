eff396532bd719f2af32d38b1cc04e40afe536d2
emirosman <m.emirosman@gmail.com>
2023-11-13 00:14:50 +0300

    Create Brand Entity

---
+++ nArchitectureDemo/src/RentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,18 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Brand : Entity<Guid>
+{
+    public string Name { get; set; }
+
+	public Brand()
+	{
+
+	}
+
+	public Brand(Guid id, string name)
+	{
+		Name= name;
+	}
+}
