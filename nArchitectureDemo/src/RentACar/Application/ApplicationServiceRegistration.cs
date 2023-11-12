588ac7173a7fd69e08d6c172c7dd127f57bd9e4b
emirosman <m.emirosman@gmail.com>
2023-11-13 01:17:33 +0300

    Services extension -> AddApplicationServices

---
+++ nArchitectureDemo/src/RentACar/Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices (this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
+        });
+
+        return services;
+    }
+}
