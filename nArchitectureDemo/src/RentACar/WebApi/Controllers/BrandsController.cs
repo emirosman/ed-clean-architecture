5d4a0a571fc8eb50b0111d43f6f8cc503c59d3e1
emirosman <m.emirosman@gmail.com>
2023-11-13 01:13:24 +0300

    Add Brand API

---
+++ nArchitectureDemo/src/RentACar/WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Features.Brands.Commands.Create;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody]CreateBrandCommand createBrandCommand)
+        {
+            var response = await Mediator.Send(createBrandCommand).ConfigureAwait(false);
+            return Ok(response);
+        }
+    }
+}
